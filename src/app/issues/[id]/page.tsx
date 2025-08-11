ca311f4c6ad939eb6b169085fb653b72ea6b6c54
Ramnarayansah <ramnarayan.person@gmail.com>
2025-08-11 08:39:47 +0545

    add reactmarkdown but not propely fix problem in  require('@tailwindcss/typography') plugin

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { prisma } from "../../../../prisma/client"
 import IssueStatusBadge from "@/app/component/IssueStatusBadge";
 import { Card, Flex } from "@radix-ui/themes";
+import ReactMarkdown from 'react-markdown'
 
 interface Props{
     params:{id:string}
@@ -23,8 +24,10 @@ export default async function page( {params}: Props){
             <IssueStatusBadge status={issue.status}/>
             <p>{issue.createdAt.toDateString()}</p>
             </Flex>
-            <Card className="mt-2">
-            <p>{issue.description}</p>
+            <Card className="mt-2 prose">
+                    
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            
 
             </Card>
         </div>
