be0042b3585ef10ab3324640170c1dafaf28e025
Ramnarayansah <ramnarayan.person@gmail.com>
2025-09-21 07:06:50 +0545

    react query api

--- src/app/QueryClientProvider.tsx
+++ src/app/QueryClientProvider.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 
 export default function QueryClientProvider({children}: {children: ReactNode}){
 const queryClient = new QueryClient();
-    
+     
     return(
     <ReactQueryClientProvider client={queryClient}>{children}</ReactQueryClientProvider>
 )
--- src/app/issues/[id]/SelectDemo.tsx
+++ src/app/issues/[id]/SelectDemo.tsx
@@ -9,6 +9,8 @@ import {
 } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@radix-ui/themes/components/skeleton";
 
 // Define User type
 interface User {
@@ -17,17 +19,16 @@ interface User {
 }
 
 const SelectDemo = () => {
-	const [users, setUsers] = useState<User[]>([]);
-
-	useEffect(() => {
-		const fetchUsers = async () => {
-			const { data } = await axios.get<User[]>('/api/users');
-			setUsers(data);
-		};
-		fetchUsers();
-	}, []);
+	const {data: users, error, isLoading} = useQuery<User[]>({
+		queryKey: ['users'],
+		queryFn: () => axios.get('/api/users').then(res => res.data),
+		staleTime: 60 * 1000,
+		retry:3
+	});
+	if(isLoading) return <Skeleton/>
 
 	return (
+		
 		<Select.Root>
 			<Select.Trigger
 				className="inline-flex items-center justify-between gap-2 rounded border border-gray-300 bg-white px-3 py-2 text-sm leading-none text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 data-[placeholder]:text-gray-400 w-56"
@@ -51,7 +52,7 @@ const SelectDemo = () => {
 							<Select.Label className="px-2 py-1 text-xs font-medium text-gray-500">
 								Suggestion
 							</Select.Label>
-							{users.map((user: User) => (
+							{users?.map((user: User) => (
 								<SelectItem key={user.id} value={user.id}>{user.name}</SelectItem>
 							))}
 						</Select.Group>
