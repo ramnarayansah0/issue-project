26c176ad64d41f634fea71c6224b1b9c635921b7
Ramnarayansah <ramnarayan.person@gmail.com>
2025-09-25 19:35:58 +0545

    Enhance api

--- src/app/ValidationSchema.ts
+++ src/app/ValidationSchema.ts
@@ -2,5 +2,10 @@ import { z } from "zod";
 
 export const createIssueSchema = z.object({
     title: z.string().min(1, "Title is required").max(255),
-    description: z.string().min(1, "Description is required")
+    description: z.string().min(1, "Description is required").max(65535) 
+});
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, "Title is required").max(255).optional(),
+    description: z.string().max(65535).min(1, "Description is required").optional(),
+    AssignedToUserId: z.string().min(1,"AssignedToUserId is required.").max(255).optional().nullable()
 });
--- src/app/api/issues/[id]/route.ts
+++ src/app/api/issues/[id]/route.ts
@@ -1,17 +1,26 @@
-import { createIssueSchema } from "@/app/ValidationSchema";
+import { patchIssueSchema } from "@/app/ValidationSchema";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../../prisma/client";
 import { getServerSession } from "next-auth/next";
 import authOptions from "@/app/auth/authOptions";
+import { error } from "console";
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
     const body = await request.json();
     const session = await getServerSession(authOptions)
     if (!session)
         return NextResponse.json({}, { status: 401 });
-    const validation = createIssueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.format, { status: 40 })
+
+    
+    if(body.AssignedToUserId){ 
+      const user = await  prisma.user.findUnique({ where : {id: body.AssignedToUserId}})
+    if(!user)
+        return NextResponse.json({error:"Invalid user."},{status:400})
+    
+    }
 
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(params.id) }
@@ -23,7 +32,8 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
         where: { id: issue.id },
         data: {
             title: body.title,
-            description: body.description
+            description: body.description,
+            assignedToUserId: body.AssignedToUserId,
         }
     })
     return NextResponse.json(updatedIssue)
