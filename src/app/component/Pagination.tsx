a922298350eb8402c86f06fc46d250142b14ec51
workramsah <workramsah@gmail.com>
2025-10-09 08:04:40 +0545

    fix build the LatestIssue component

--- src/app/component/Pagination.tsx
+++ src/app/component/Pagination.tsx
@@ -1,5 +1,7 @@
+"use client"
 import { ArrowLeftIcon, ArrowRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from "@radix-ui/react-icons";
 import {Text, Flex, Button } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface Props{
     itemCount: number;
@@ -8,31 +10,39 @@ interface Props{
 }
 
 export default function Pagination({itemCount, pageSize, currentPage}: Props){
+    const router = useRouter();
+    const searchParams = useSearchParams();
     
     const pageCount = Math.ceil(itemCount / pageSize);
     if(pageCount <= 1) return null;
+    
+    const changePage = (page: number) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', page.toString());
+        router.push(`?${params.toString()}`);
+    }
 
     return(
         <>
         <Flex className="items-center gap-3">
             <Text>Page {currentPage} of {pageCount}</Text>
-            <Button variant="soft" disabled={currentPage === 1}>
+            <Button variant="soft" disabled={currentPage === 1} onClick={()=>changePage(1)}>
                 <DoubleArrowLeftIcon ></DoubleArrowLeftIcon>
             </Button>
 
 
 
-            <Button variant="soft" disabled={currentPage === 1}>
+            <Button variant="soft" disabled={currentPage === 1} onClick={()=>changePage(currentPage - 1)}>
             <ArrowLeftIcon></ArrowLeftIcon>
             </Button>
 
 
-            <Button variant="soft" disabled={currentPage === pageCount}>
+            <Button variant="soft" disabled={currentPage === pageCount} onClick={()=>changePage(currentPage + 1)}>
             <ArrowRightIcon></ArrowRightIcon>
             </Button>
 
 
-            <Button variant="soft" disabled={currentPage === pageCount}>
+            <Button variant="soft" disabled={currentPage === pageCount} onClick={()=>changePage(pageCount)}>
             <DoubleArrowRightIcon></DoubleArrowRightIcon>
             </Button>
         </Flex>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,48 @@
-import Image from "next/image";
-import Pagination from "./component/Pagination";
 
-export default function Home() {
+"use client"
+import Pagination from "./component/Pagination";
+import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import IssueStatusBadge from "./component/IssueStatusBadge";
+import { Status } from "@/generated/prisma/client";
+import { prisma } from "../../prisma/client";
+import { AvatarIcon } from "@radix-ui/react-icons";
+
+export default async function Home () {
+  const issues =  await prisma.issue.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: 5,
+    include:{
+      assignedToUser:true
+    }
+  });
   return (
     <>
     <div>home page </div>
     <Pagination itemCount={100} pageSize={10} currentPage={1}/> 
+    <Card>
+      <Heading size={"2"}>Latest Issues</Heading>
+    <Table.Root>
+      <Table.Body>
+          {issues.map(issue=>(
+        <Table.Row>
+
+          <Table.Cell key={issue.id}>
+            <Flex justify={"between"}>
+         <Flex direction="column" align={"start"}>
+          <Link href={`/issues/${1}`}>{issue.title}</Link>
+          <IssueStatusBadge status={issue.status}></IssueStatusBadge>
+         </Flex>
+         {issue.assignedToUser &&(
+          <Avatar src={issue.assignedToUser.image!} fallback="?"></Avatar>
+          )} 
+         </Flex>
+         </Table.Cell>
+        </Table.Row>
+          ))}
+      </Table.Body>
+    </Table.Root>
+    </Card>
     </>
 
   );
